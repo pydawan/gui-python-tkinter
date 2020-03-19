6b201734447b0929137e77bb3dd2b7ad2eb3e59c
natorsc <natorsc@gmail.com>
2020-03-19 01:18:49 -0300

    Melhorando o código para Linux e Windows.

--- src/gen-exec/cxfreeze/setup.py
+++ src/gen-exec/cxfreeze/setup.py
@@ -1,44 +1,35 @@
 # -*- coding: utf-8 -*-
-"""."""
-import os
+"""Gerando executáveis com Cx_Freeze!"""
+
 import sys
 import tkinter as tk
+from os import environ
 from pathlib import Path
 
-from cx_Freeze import Executable, setup
+from cx_Freeze import setup, Executable
 
 root = tk.Tk()
-
-tcl_lib_path = Path(root.tk.exprstring('$tcl_library'))
-tk_lib_path = Path(root.tk.exprstring('$tk_library'))
-
-os.environ['TCL_LIBRARY'] = str(tcl_lib_path)
-os.environ['TK_LIBRARY'] = str(tk_lib_path)
-
-base = None
+lib_tcl_path = Path(root.tk.exprstring('$tcl_library'))
+lib_tk_path = Path(root.tk.exprstring('$tk_library'))
+environ['TCL_LIBRARY'] = str(lib_tcl_path)
+environ['TK_LIBRARY'] = str(lib_tk_path)
 
 build_exe_options = {
-    'packages': ['tkinter'],
+    'excludes': ['wx', 'email', 'pydoc_data', 'curses'],
     'include_files': ['icons'],
+    'packages': ['tkinter'],
 }
 
-# Windows.
+base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
-
-    build_exe_options = {
-        'packages': ['tkinter'],
-        'include_files': [
-            # Ajustar para o local onde o Python está instalado no Windows!
-            os.path.join('C:\\Python37\\DLLs', 'tcl86t.dll'),
-            os.path.join('C:\\Python37\\DLLs', 'tk86t.dll'),
-            'icons',
-        ]
-    }
+if sys.platform == 'win64':
+    base = 'Win64GUI'
 
 setup(
     name='MyApp',
-    version='0.1',
+    author='Renato Cruz (natorsc@gmail.com)',
+    version='0.0.1',
     description='Criando executáveis com Cx_Freeze!',
     options={'build_exe': build_exe_options},
     executables=[
